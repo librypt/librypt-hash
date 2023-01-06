a2b2a4122cced1ab6dbf65be2e07cd792b92d407
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-06 11:19:14 -0800

    Modified API to be less dependent on const generics.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
-/// The raw bytes for a hash.
-pub type Hash<const LENGTH: usize> = [u8; LENGTH];
+/// A generic hash that can be referenced as slice of bytes.
+pub trait Hash: AsRef<[u8]> {}
 
-/// A hasher that produces a `Hash` of HASH_LENGTH from a given message.
-pub trait Hasher<const HASH_LENGTH: usize> {
+/// A hasher that produces a `Hash` of OUTPUT_LENGTH bytes from a given message.
+pub trait Hasher {
+    /// NOTE: This is in bytes, not bits.
+    const BLOCK_LENGTH: usize;
+
+    /// NOTE: This is in bytes, not bits.
+    const OUTPUT_LENGTH: usize;
+
+    /// NOTE: A `Hasher` can return any custom type for the hash, but it must implement AsRef<[u8]> so users can get the hash as a raw slice of bytes.
+    type Output: Hash;
+
+    /// Update the input state of the hasher with the provided message bytes.
     fn update(&mut self, msg: &[u8]);
 
-    fn finalize(self) -> Hash<HASH_LENGTH>;
+    /// Compute the final hash with the hasher state.
+    fn finalize(self) -> Self::Output;
 
-    fn hash(msg: &[u8]) -> Hash<HASH_LENGTH>;
+    /// Stateless hashing of a given message.
+    fn hash(msg: &[u8]) -> Self::Output;
 }
