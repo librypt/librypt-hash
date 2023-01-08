5ebee4fc322d420fc7a1993094e67b22bdea7b14
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-07 18:55:12 -0800

    Simplified hash traits and types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,8 @@
-/// A generic hash that can be referenced as slice of bytes.
+/// A hash represented by a fixed array of bytes.
 pub type Hash<const SIZE: usize> = [u8; SIZE];
 
-/// A hasher that produces a `Hash` of OUTPUT_SIZE bytes from a given message.
-pub trait Hasher<const BLOCK_SIZE: usize, const OUTPUT_SIZE: usize> {
-    /// Initialize incremental hasher.
-    fn new() -> Self;
-
-    /// Update the input state of the hasher with the provided message bytes.
-    fn update(&mut self, msg: &[u8]);
-
-    /// Compute the final hash with the hasher state.
-    fn finalize(self) -> Hash<OUTPUT_SIZE>;
-
-    /// Compute the final hash with the hasher state, clearing the state for reuse.
-    fn finalize_reset(&mut self) -> Hash<OUTPUT_SIZE>;
-
-    /// Stateless hashing of a given message.
+/// A hash function that produces a `Hash` of OUTPUT_SIZE bytes from a given message.
+pub trait HashFn<const BLOCK_SIZE: usize, const OUTPUT_SIZE: usize> {
+    /// Compute the has for the given message.
     fn hash(msg: &[u8]) -> Hash<OUTPUT_SIZE>;
 }
