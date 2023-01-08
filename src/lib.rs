28f5ed96c27a16178b51bb55fc291b8267af6d83
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-07 20:57:34 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ pub type Hash<const SIZE: usize> = [u8; SIZE];
 /// A hash function that produces a `Hash` of OUTPUT_SIZE bytes from a given message.
 pub trait HashFn<const BLOCK_SIZE: usize, const OUTPUT_SIZE: usize> {
     /// Compute the has for the given message.
-    fn hash(msg: impl IntoIterator) -> Hash<OUTPUT_SIZE>;
+    fn hash(msg: &[u8]) -> Hash<OUTPUT_SIZE>;
 }
