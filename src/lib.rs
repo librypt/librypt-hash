cc67326fdfda5728fd7731d8188afc8399372599
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-07 17:29:36 -0800

    Added a couple new trait requirements to Hasher.

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,17 @@ pub type Hash<const SIZE: usize> = [u8; SIZE];
 
 /// A hasher that produces a `Hash` of OUTPUT_SIZE bytes from a given message.
 pub trait Hasher<const BLOCK_SIZE: usize, const OUTPUT_SIZE: usize> {
+    /// Initialize incremental hasher.
+    fn new() -> Self;
+
     /// Update the input state of the hasher with the provided message bytes.
     fn update(&mut self, msg: &[u8]);
 
     /// Compute the final hash with the hasher state.
     fn finalize(self) -> Hash<OUTPUT_SIZE>;
+
+    /// Compute the final hash with the hasher state, clearing the state for reuse.
+    fn finalize_reset(&mut self) -> Hash<OUTPUT_SIZE>;
 
     /// Stateless hashing of a given message.
     fn hash(msg: &[u8]) -> Hash<OUTPUT_SIZE>;
