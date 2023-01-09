e0cd79e814068f1e8b2a8f3a95f80470dd1cb474
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-08 17:27:56 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,18 @@ pub type Hash<const SIZE: usize> = [u8; SIZE];
 
 /// A hash function that produces a `Hash` of OUTPUT_SIZE bytes from a given message.
 pub trait HashFn<const BLOCK_SIZE: usize, const OUTPUT_SIZE: usize> {
+    /// Initialize hash state.
+    fn new() -> Self;
+
+    /// Update the hash state with the given data.
+    fn update(&mut self, data: &[u8]);
+
+    /// Consumes the hash state, producing the final hash value.
+    fn finalize(self) -> Hash<OUTPUT_SIZE>;
+
+    /// Produces the final hash value, resetting the hash state for reuse.
+    fn finalize_reset(&mut self) -> Hash<OUTPUT_SIZE>;
+
     /// Compute the has for the given message.
     fn hash(msg: &[u8]) -> Hash<OUTPUT_SIZE>;
 }
