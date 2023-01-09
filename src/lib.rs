df68cb639f95305c3179eed4bc69f712f2196170
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-08 19:54:01 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-/// A hash represented by a fixed array of bytes.
-pub type Hash<const SIZE: usize> = [u8; SIZE];
+/// A hash that can be referenced as a fixed array of bytes.
+pub trait Hash<const SIZE: usize>: AsRef<[u8; SIZE]> {}
 
 /// A hash function that produces a `Hash` of OUTPUT_SIZE bytes from a given message.
-pub trait HashFn<const BLOCK_SIZE: usize, const OUTPUT_SIZE: usize> {
+pub trait HashFn<const BLOCK_SIZE: usize, const OUTPUT_SIZE: usize>: Sized {
+    /// Custom output hash type (must implement `Hash` trait).
+    type Output: Hash<OUTPUT_SIZE>;
+
     /// Initialize hash state.
     fn new() -> Self;
 
@@ -10,11 +13,17 @@ pub trait HashFn<const BLOCK_SIZE: usize, const OUTPUT_SIZE: usize> {
     fn update(&mut self, data: &[u8]);
 
     /// Consumes the hash state, producing the final hash value.
-    fn finalize(self) -> Hash<OUTPUT_SIZE>;
+    fn finalize(self) -> Self::Output;
 
     /// Produces the final hash value, resetting the hash state for reuse.
-    fn finalize_reset(&mut self) -> Hash<OUTPUT_SIZE>;
+    fn finalize_reset(&mut self) -> Self::Output;
 
     /// Compute the has for the given message.
-    fn hash(msg: &[u8]) -> Hash<OUTPUT_SIZE>;
+    fn hash(msg: &[u8]) -> Self::Output {
+        let mut hasher = Self::new();
+
+        hasher.update(msg);
+
+        hasher.finalize()
+    }
 }
